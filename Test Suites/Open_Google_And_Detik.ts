16c99f98908800942bbce41098cee82fd486ec84
Ferdiando Gondokusumo <fgondokusumo@intikom.co.id>
2026-03-30 15:53:46 +0700

    add test suites Open_Google_And_Detik

---
+++ Test Suites/Open_Google_And_Detik.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Open_Google_And_Detik</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>10</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>73dc18de-7d68-4127-8770-10a4ec872c9e</testSuiteGuid>
+</TestSuiteEntity>
